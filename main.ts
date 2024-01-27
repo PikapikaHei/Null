7683e6ba37acba820829079e53a3647898c77f1c
PikapikaHei <157876191+PikapikaHei@users.noreply.github.com>
2024-01-27 15:43:17 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,13 @@
+/**
+ * DATA
+ */
+//% weight=1 color=#808080 icon=""
+namespace data {
+    /**
+     * Returns null
+     */
+    //% block="null" weight=1000
+    export function nullValue(): null {
+        return null;
+    }
+}
